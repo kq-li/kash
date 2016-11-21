710e77a2c89816086ce57ac724899c1aecf1ff9c
Kenneth Li <kennethli.3470@gmail.com>
2016-11-21 15:34:44 -0500

    Rudimentary functionaliy complete

--- shell.c
+++ shell.c
@@ -2,8 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+
+#define INPUT_MAX 1024
+
+char **parse(char input[INPUT_MAX]) {
+	char *s = input;
+	s = strsep(&s, "\n");
+	
+	char **command = (char **) malloc(sizeof(char *) * INPUT_MAX / 2 + 1);
+	int i = 0;
+
+	while (command[i++] = strsep(&s, " "));
+
+	command[i] = 0;
+	
+	return command;
+}
+
+void runNext() {
+	printf("@ ");
+	char input[INPUT_MAX];
+	fgets(input, sizeof(char) * INPUT_MAX, stdin);
+	char **command = parse(input);
+
+	if (fork()) {
+		wait(NULL);
+	} else {
+		execvp(command[0], command);
+	} 
+}
+
+static void sighandler(int signo) {
+	switch (signo) {
+	case SIGINT:
+		exit(-1);
+		break;
+	}
+}
 
 int main() {
-
+	int isRunning = 1;
+	signal(SIGINT, sighandler);
+	
+	while (isRunning) {
+		runNext();
+	}
+	
 	return 0;
 }
